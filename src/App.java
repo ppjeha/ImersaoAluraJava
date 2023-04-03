da66ca8ea64dc2d508c97767096af134d251f0f9
Pedro Paulo Jeha Bonaldo <pepajeha@gmail.com>
2023-04-03 15:41:45 -0300

    -Implementado aula 3 com refatoracao para abstrair extratores de diferentes api's;

--- src/App.java
+++ src/App.java
@@ -1,45 +1,38 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        
+    public static void main(String[] args) throws Exception {    
+        // String urlImdb = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // //Fazer o get do arquivo atraves de uma conexao HTTP
+        // String data = new AppHttpClient().getData(urlImdb);
+
+        String urlNasa = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
         //Fazer o get do arquivo atraves de uma conexao HTTP
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .GET()
-            .build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String data = new AppHttpClient().getData(urlNasa);
 
         //Parse dos dados do JSON (titulo, poster, rating)
-        List<Map<String, String>> listaDeFilmes = new JsonParser().parse(response.body());
+        NasaContentExtractor nasaExtractor = new NasaContentExtractor();
+        List<Content> contentList = nasaExtractor.extractContent(data);
 
         //Exibir e manipular os dados
         ImageGenerator imgGenerator = new ImageGenerator();
-        for (Map<String,String> filme : listaDeFilmes) {
-            InputStream is = new URL(filme.get("image")).openStream();
-            String title = filme.get("title").replaceAll("[^a-zA-Z0-9 ]", "") + ".png";
-            Double rating = Double.parseDouble(filme.get("imDbRating"));
-            String text;
+        for (Content item : contentList) {
+            InputStream is = new URL(item.getUrlImage()).openStream();
+            String title = item.getTitle().replaceAll("[^a-zA-Z0-9 ]", "") + ".png";
+            // Double rating = Double.parseDouble(item.get("imDbRating"));
+            String text = "TOPZERA";
 
-            if (rating >= 9.0) {
-                text=  "TOPZERA";
-            }else{
-                text = "BEM OK";
-            }
+            // if (rating >= 9.0) {
+            //     text=  "TOPZERA";
+            // }else{
+            //     text = "BEM OK";
+            // }
                     
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            System.out.println(item.getTitle());
+            System.out.println(item.getUrlImage());
+            // System.out.println(item.get("imDbRating"));
 
             imgGenerator.generate(is, title, text);
         }
---
+++ src/IContentExtractor.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public interface IContentExtractor {
+    public List<Content> extractContent(String json);
+}
