cb55ca4c369ddb10f28e1b5531d5a93012f8af29
Pedro Paulo Jeha Bonaldo <pepajeha@gmail.com>
2023-04-03 13:35:32 -0300

    -Implementado a proposta da aula 2 com os desafios;

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -22,10 +24,24 @@ public class App {
         List<Map<String, String>> listaDeFilmes = new JsonParser().parse(response.body());
 
         //Exibir e manipular os dados
+        ImageGenerator imgGenerator = new ImageGenerator();
         for (Map<String,String> filme : listaDeFilmes) {
+            InputStream is = new URL(filme.get("image")).openStream();
+            String title = filme.get("title").replaceAll("[^a-zA-Z0-9 ]", "") + ".png";
+            Double rating = Double.parseDouble(filme.get("imDbRating"));
+            String text;
+
+            if (rating >= 9.0) {
+                text=  "TOPZERA";
+            }else{
+                text = "BEM OK";
+            }
+                    
             System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
             System.out.println(filme.get("imDbRating"));
+
+            imgGenerator.generate(is, title, text);
         }
 
     }
