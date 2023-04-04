60978123e0e829eb02b25d6196dd795823837b5b
Pedro Paulo Jeha Bonaldo <pepajeha@gmail.com>
2023-04-04 00:47:37 -0300

    - Adicionado metodo POST no endpoint /linguagens;

--- linguagens-api/src/main/java/br/com/java/linguagens/api/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/java/linguagens/api/LinguagemController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +27,11 @@ public class LinguagemController {
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens(){
         return repository.findAll();
+    }
+
+    @PostMapping
+    public Linguagem gravaLinguagem(@RequestBody Linguagem linguagem) {
+        return repository.save(linguagem);
     }
 
 }
